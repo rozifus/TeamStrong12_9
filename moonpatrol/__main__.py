ca59b324331dd6e186af6a8cf7056fe6cae27783
Jervis Whitley <jervis.whitley+git@gmail.com>
2012-09-09 20:05:39 +1000

    changed requirement ot pygame and added a basic app.

--- moonpatrol/__main__.py
+++ moonpatrol/__main__.py
@@ -1,4 +1,18 @@
+import sys
+import time
+import random
+
+import pygame
 
 def main():
     """ your app starts here
     """
+    pygame.init()
+    screen = pygame.display.set_mode((320, 240))
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        time.sleep(0.1)
+        screen.fill((0, random.randint(1, 255), 0))
+        pygame.display.flip()
