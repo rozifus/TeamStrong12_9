ea8583cb6cd2ddae9dd2ce655ca3a258321b9643
Jervis Whitley <jervis.whitley+git@gmail.com>
2012-09-09 20:13:33 +1000

    added a settings file. Made a screen with old school flashing green luminous background.

--- moonpatrol/__main__.py
+++ moonpatrol/__main__.py
@@ -4,15 +4,17 @@ import random
 
 import pygame
 
+import settings
+
 def main():
     """ your app starts here
     """
     pygame.init()
-    screen = pygame.display.set_mode((320, 240))
+    screen = pygame.display.set_mode(settings.DISPLAY_SIZE)
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
         time.sleep(0.1)
-        screen.fill((0, random.randint(1, 255), 0))
+        screen.fill((0, random.randint(*settings.BACKGROUND_FLICKER), 0))
         pygame.display.flip()
