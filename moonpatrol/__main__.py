0d213f80c74e6b64630ed5529805e2c9f2f4cb93
Jervis Whitley <jervis.whitley+git@gmail.com>
2012-09-10 14:30:35 +1000

    can move left and right and can jump too!

--- moonpatrol/__main__.py
+++ moonpatrol/__main__.py
@@ -7,6 +7,10 @@ import pygame
 from data import load, filepath
 import settings
 
+JUMP = getattr(pygame, settings.JUMP)
+SPEEDUP = getattr(pygame, settings.SPEEDUP)
+SLOWDOWN = getattr(pygame, settings.SLOWDOWN)
+
 class Background(object):
 
     def __init__(self, image, maxx, scrollspeed):
@@ -30,6 +34,40 @@ class Background(object):
         screen.blit(self._image, (self._x1, 0))
         screen.blit(self._image, (self._x2, 0))
 
+class Car(object):
+    def __init__(self, image, groundy):
+        self._speed = 0
+        self._image = image.convert_alpha()
+        self._x = 100
+        self._xspeed = 0
+        self._y = groundy
+        self._yspeed = 0
+        self._groundy = groundy
+        self._jumping = False
+
+    def change_speed(self, direction):
+        self._xspeed += direction
+        self._xspeed = max(self._xspeed, -1)
+        self._xspeed = min(self._xspeed, 1)
+
+    def jump(self, force=50):
+        if not self._jumping:
+            self._yspeed = -force
+            self._jumping = True
+
+    def update(self):
+        self._yspeed += settings.GRAVITY
+        self._y += self._yspeed
+        if self._y >= self._groundy:
+            self._yspeed = 0
+            self._y = self._groundy
+            self._jumping = False
+
+        self._x += self._xspeed
+
+    def render(self, screen):
+        screen.blit(self._image, (self._x, self._y))
+
 def main():
     """ your app starts here
     """
@@ -38,23 +76,26 @@ def main():
     screen = pygame.display.set_mode(settings.DISPLAY_SIZE)
     _bground = pygame.image.load(filepath('bground.png'))
     _car = pygame.image.load(filepath('patrol.png'))
-    car = _car.convert_alpha()
+    car = Car(_car, 400)
     bground = pygame.transform.scale(_bground, settings.DISPLAY_SIZE)
 
     background = Background(bground, 
                     settings.DISPLAY_SIZE[0], settings.SCROLL_SPEED)
 
     clock = pygame.time.Clock()
-    carx = 0
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == JUMP: car.jump()
+                if event.key == SPEEDUP: car.change_speed(1)
+                if event.key == SLOWDOWN: car.change_speed(-1)
 
         clock.tick(60)
         # blit first bit.
         screen.fill(settings.BLACK)
         background.render(screen)
-        screen.blit(car, (carx, 400))
-        carx += 1
+        car.update()
+        car.render(screen)
         pygame.display.flip()
